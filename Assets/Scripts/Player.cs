4b3974220808e031b57a4badf1aa35f3995799e7
Oben Ozgur <ozguro18@itu.edu.tr>
2023-11-10 14:50:40 +0200

    Clean Player.cs code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,53 +6,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float TOLERANCE = 0.5f;
+    private const float PositionTolerance = 0.5f;
     public LeftArm leftArm;
     public LeftLeg leftLeg;
     
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Moi moi!");
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
 
         var transformPosition = transform.position;
         
-        if (transformPosition.z >= 15 - TOLERANCE && transformPosition.z <= 15 + TOLERANCE)
+        if (transformPosition.z >= 15 - PositionTolerance && transformPosition.z <= 15 + PositionTolerance)
         {
-            Debug.Log("Teleporting!");
             transformPosition.z = 0;
             transform.position = transformPosition;
-            leftArm.leftArmMarked = false;
-            leftLeg.leftLegMarked = false;
-
+            leftArm.isMarked = false;
+            leftLeg.isMarked = false;
         }
     }
     
     void OnTriggerEnter(Collider other)
     {
-        // Check if the colliding object has a specific tag
         if (other.gameObject.CompareTag("Wall") && other.gameObject.layer == LayerMask.NameToLayer("Default"))
         {
-            Debug.Log("Wall");
-            if (!leftArm.leftArmMarked)
+            if (!leftArm.isMarked)
             {
                 leftArm.gameObject.SetActive(false);
             }
-            else if (!leftLeg.leftLegMarked)
+            
+            if (!leftLeg.isMarked)
             {
-                Debug.Log("Not Marked");
                 leftLeg.gameObject.SetActive(false);
             }
-            
         }
     }
-
 }
