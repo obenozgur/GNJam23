1b5b6ad4ff06f7792c7108f4559b870c53990ed4
Oben Ozgur <ozguro18@itu.edu.tr>
2023-11-11 21:34:04 +0200

    Add wall background update

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private const int WallCount = 8;
+    private Sprite[] spriteArray = new Sprite[WallCount];
+    private int currentWallIndex;
+    void Start()
+    {
+        for (int i = 0; i < WallCount; i++)
+        {
+            string spritePath = $"Wall/dodging_wall_({i+1})";
+            spriteArray[i] = Resources.Load<Sprite>(spritePath);
+        }
+        
+    }
+    
+    public void UpdateWall()
+    {
+        int wallIndex = Random.Range(0, WallCount);
+        
+        while (wallIndex == currentWallIndex)
+        {
+            wallIndex = Random.Range(0, WallCount);
+        }
+
+        currentWallIndex = wallIndex;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null && spriteArray[wallIndex] != null)
+        {
+            spriteRenderer.sprite = spriteArray[wallIndex];
+        }
+    }
+}
